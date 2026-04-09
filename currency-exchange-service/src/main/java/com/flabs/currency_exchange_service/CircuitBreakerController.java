562ee8121f4385ecd65982432075142ae600908d
salog0d <david.salomon.nava11@gmail.com>
2026-04-09 09:59:48 -0600

    added bulkhead calls feature

--- currency-exchange-service/src/main/java/com/flabs/currency_exchange_service/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/flabs/currency_exchange_service/CircuitBreakerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
@@ -19,6 +20,7 @@ public class CircuitBreakerController {
     // @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
     @CircuitBreaker(name = "sample-api", fallbackMethod = "hardcodedResponse")
     @RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     // 10s => 10000 calls to sample api
     public String sampleApi() {
         logger.info("Sample Api call received");
